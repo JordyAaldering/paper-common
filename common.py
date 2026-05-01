68e24c01e79313ec3478d419b1f1e5cc9c014686
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-05-01 12:48:09 +0200

    extra_axis_parameters

--- common.py
+++ common.py
@@ -22,6 +22,7 @@ def as_tikz(fig, ax, ax2=None,
             axis_width: str = r'\linewidth',
             axis_height: str = r'.8\linewidth',
             y_axis_text_size: str = 'scriptsize',
+            extra_axis_parameters = dict(),
             **kwargs
         ) -> str:
 
@@ -33,6 +34,7 @@ def as_tikz(fig, ax, ax2=None,
             axis_width=axis_width,
             axis_height=axis_height,
             include_disclaimer=False,
+            extra_axis_parameters=extra_axis_parameters,
             **kwargs)
 
     code = tikz_sanitize_labels(code)
